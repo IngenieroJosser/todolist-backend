d38b0592e7f7cf0dd9af048d5eb332b7853a93d7
Joss <cordobarivasjosser@gmail.com>
2025-08-17 01:53:49 -0500

    feat: añadiendo migraciones

--- models/user.go
+++ models/user.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
     gorm.Model
-    Name  string `json:"name"`
-    Email string `json:"email" gorm:"unique"`
-		Age   int    `json:"age"`
-		
+    Name  				string `json:"name"`
+    Email 				string `json:"email" gorm:"unique"`
+		Password      string `json:"password"`
+		Age           int    `json:"age"`
 }
